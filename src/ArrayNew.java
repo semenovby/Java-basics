f7c32b17c1980d110d640fabe357a3fa2881f815
semenovby <semenovby@mail.ru>
2021-12-19 22:42:34 +0300

    working code

--- src/ArrayNew.java
+++ src/ArrayNew.java
@@ -1,6 +1,8 @@
+import java.awt.image.AreaAveragingScaleFilter;
+
 public class ArrayNew {
     public static void main(String[] args) {
-        int[] Array = new int[10];                                           //создание массива
+        int[] Array = new int[10];                                                      //создание массива
         Array[0] = 8;
         Array[1] = 6;
         Array[2] = 15;
@@ -12,17 +14,35 @@ public class ArrayNew {
         Array[8] = 7;
         Array[9] = 9;
 
-        int finder = 8;                                                       //поиск знаения ячейки
+
+        int finder = 8;                                                                 //поиск знаения ячейки
+        int counter = 0;                                                                //счетчик совпадений
+
+             //проблема со счетчиком(всегда 0)  //создание конечного массива
+
         for (int cell = 0; cell < 10; cell++) {
-            System.out.println(Array[cell]);
-            if (Array[cell] == finder) {                                      //совпадение искомого значения
-                for (int slider = cell; slider < 9; slider++) {               //сдвиг массива влево
+            if (Array[cell] == finder) {                                                //совпадение искомого значения
+                ++counter;
+                for (int slider = cell; slider < 9; slider++) {                         //сдвиг массива влево
                     Array[slider] = Array[slider + 1];
                 }
+
+
             }
         }
-        for (int cell = 0; cell < 10; cell++) {
-            System.out.println("ячейка [" + cell + "] имеет значение " + Array[cell]); //вывод результатов
+        int[] NewArray = new int[10 - counter];
+        for (int newCell = 0; newCell < 10 - counter; newCell++) {              //присвоение в новый массив
+            NewArray[newCell] = Array[newCell];
+        }
+        System.out.println(counter);
+        System.out.println("Старый массив:");
+        for (int i = 0; i < 10; i++) {                                                  //вывод результатов
+            System.out.println("ячейка [" + i + "] имеет значение " + Array[i]);
+        }
+        System.out.println("Новый массив:");
+        for (int i = 0; i < NewArray.length; i++) {                                          //вывод результатов
+            System.out.println("ячейка [" + i + "] имеет значение " + NewArray[i]);
         }
     }
 }
+
