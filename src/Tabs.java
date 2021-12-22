677594a60a3cbc53fbd540afa8146bade41796de
semenovby <semenovby@mail.ru>
2021-12-22 17:31:45 +0300

    upload

---
+++ src/Tabs.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Tabs {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Для активации программы введи любое целое число и нажми ENTER: ");
+        in.nextInt();
+        int countGoodAnswer = 0;
+        for (int i = 0; i < 10; i++) {
+            int a = (int) (Math.random() * 10);
+            int b = (int) (Math.random() * 10);
+            System.out.print(a + "х" + b + "= ");
+            if (a * b == in.nextInt()) {
+                System.out.println("Молодец!Правильно!");
+                countGoodAnswer++;
+            } else {
+                System.out.println("ай-яй-яй-яй! " + a + "*" + b + "= " + a * b);
+            }
+        }
+        in.close();
+        if (countGoodAnswer > 10) {
+            System.out.println("Правильных ответов: " + countGoodAnswer);
+            System.out.println("Отлично!");
+        } else if ((countGoodAnswer >= 8) && (countGoodAnswer <= 9)) {
+            System.out.println("Правильных ответов: " + countGoodAnswer);
+            System.out.println("Хорошо!");
+        } else if ((countGoodAnswer >= 6) && (countGoodAnswer < 8)) {
+            System.out.println("Неравильных ответов: " + countGoodAnswer);
+            System.out.println("Удовлетворительно!");
+        } else if (countGoodAnswer <= 5) {
+            System.out.println("Неравильных ответов: " + countGoodAnswer);
+            System.out.println("Удовлетворительно!");
+        }
+    }
+}
