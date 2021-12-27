ca255af4f8c03bb8ecb2f1d440df9cae833e2ff4
semenovby <semenovby@mail.ru>
2021-12-27 23:47:22 +0300

    update

--- src/Bank.java
+++ src/Bank.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Bank {
     /*Мы взяли ссуду в банке под 20% годовых в объеме введенном с клавиатуры.
-    Нам интересно, через сколько лет наш долг банку превысит totalDebt, введенный с клавиатуры?
+    Нам интересно, через сколько лет наш долг банку превысит startDebt, введенный с клавиатуры?
     Решить задачу в следующих вариантах:
             1. Если мы не выплачиваем ничего вообще.
             2. Если мы платим каждый год 10% от начальной суммы.
@@ -12,31 +12,31 @@ public class Bank {
 
         Scanner in = new Scanner(System.in);
         System.out.println("Для активации программы введи любое целое число и нажми ENTER: ");
-        double totalDebt = in.nextInt();
+        double startDebt = in.nextInt();
         in.close();
-        double dolg = totalDebt;
-        System.out.println("Вы взяли кредит на сумму = " + dolg);
-        int stavka = 20;
-        int procentTotalDebt = 0;
-        int procentDolg = 10;
+        double totalDebt = startDebt;
+        System.out.println("Вы взяли кредит на сумму = " + totalDebt);
+        int bankPersent = 20;
+        int persentTotalDebt = 0;
+        int persentDolg = 10;
 
-        double raznica = 0;
+        double difference = 0;
         int countYears = 0;
-        for (int years = 1; raznica < totalDebt; years++) {
-            if (procentDolg != 0) {
-                dolg = (dolg + dolg * stavka / 100) - dolg * procentDolg / 100;
-                raznica = dolg - totalDebt;
+        for (int years = 1; difference < startDebt; years++) {
+            if (persentDolg != 0) {
+                totalDebt = (totalDebt + totalDebt * bankPersent / 100) - totalDebt * persentDolg / 100;
+                difference = totalDebt - startDebt;
                 countYears++;
-            } else if (procentTotalDebt != 0) {
-                dolg = (dolg + dolg * stavka / 100) - totalDebt * procentTotalDebt / 100;
-                raznica = dolg - totalDebt;
+            } else if (persentTotalDebt != 0) {
+                totalDebt = (totalDebt + totalDebt * bankPersent / 100) - startDebt * persentTotalDebt / 100;
+                difference = totalDebt - startDebt;
                 countYears++;
-            } else if (procentDolg == 0 && procentTotalDebt == 0) {
-                dolg = dolg + dolg * stavka / 100;
-                raznica = dolg - totalDebt;
+            } else if (persentDolg == 0 && persentTotalDebt == 0) {
+                totalDebt = totalDebt + totalDebt * bankPersent / 100;
+                difference = totalDebt - startDebt;
                 countYears++;
             }
         }
-        System.out.println("Cпустя " + countYears + " года(лет) Ваш долг банку превысил изначально взятую сумму " + totalDebt + " и составил = " + dolg);
+        System.out.println("Cпустя " + countYears + " года(лет) Ваш долг банку превысил изначально взятую сумму " + startDebt + " и составил = " + totalDebt);
     }
 }
